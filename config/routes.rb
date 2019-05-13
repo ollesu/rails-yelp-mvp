26ce3cbaaa48f47d8d51aab0886bcc01e6c0e2bc
Aisa Mukukenova <mkaysa@gmail.com>
2019-05-13 15:30:19 +0100

    Restaurant review with Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'reviews', to: 'reviews#'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Patty & Bun',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '1234567',
+    category:      'chinese'
+  },
+
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12345678',
+    category:      'italian'
+  },
+
+  {
+    name:         'Pig and Butcher',
+    address:      'Angel Islington',
+    phone_number:  '123456789',
+    category:      'japanese'
+  },
+
+  {
+    name:         'Store Street Espresso',
+    address:      'Bloomsbury Tavistock Place',
+    phone_number:  '1234567899',
+    category:      'french'
+  },
+
+  {
+    name:         'Drury Lane',
+    address:      'Stoke Newington',
+    phone_number:  '12345678998',
+    category:      'belgian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
